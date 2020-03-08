d942b0545d2c99970aadc23ebcb5d47c6bb17ffb
Loredana Moanga <loredana.moanga@container-solutions.com>
2020-03-08 18:24:56 +0100

    added assert False to test

--- test_main.py
+++ test_main.py
@@ -36,6 +36,6 @@ def test_auth(client):
                            content_type='application/json')
 
     assert response.status_code == 200
+    assert False
     token = response.json['token']
     assert token is not None
-    assert False
